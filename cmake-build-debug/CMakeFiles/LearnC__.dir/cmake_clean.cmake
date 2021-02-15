848d7b2e20d8110ebd02d135dacd3407b2dca42c
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-15 18:46:05 -0500

    Hello World

---
+++ cmake-build-debug/CMakeFiles/LearnC__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LearnC__.dir/main.cpp.o"
+  "LearnC__"
+  "LearnC__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LearnC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
