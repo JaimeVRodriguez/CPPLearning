052977d58a718257133a095395b643c752e6eebc
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-18 07:17:52 -0500

    Update Calculator

--- basicCalcualtor.cpp
+++ basicCalcualtor.cpp
@@ -2,18 +2,36 @@
 
 using namespace std;
 
-int main()
-{
-    int num1, num2;
 
-    cout << "Enter first number: ";
+int main() {
+
+    int num1, num2, result;
+    string op;
+
+    cout <<  "Enter first number: " << endl ;
     cin >> num1;
 
-    cout << "Enter second number: ";
+    cout << "Enter second number: " << endl;
     cin >> num2;
 
-    int sum = num1 + num2;
-    cout << "Sum of " << num1 << " and " << num2 << " is " << sum << endl;
+    cout << "What math operation do you want to perform" << endl;
+    cout << "( + , - , / , x)" << endl;
+    cin >> op;
+
+
+    if(op == "+") {
+        result = num1 + num2;
+    } else if(op == "-") {
+        result = num1 - num2;
+    } else if(op == "/") {
+        result = num1 / num2;
+    } else if(op == "x") {
+        result = num1 * num2;
+    } else {
+        cout << "Invalid operator" << endl;
+    }
+
+    cout << result;
 
 
 
