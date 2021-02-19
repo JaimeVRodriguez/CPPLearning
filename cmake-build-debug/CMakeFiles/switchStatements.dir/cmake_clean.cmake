ca91066897f1c48938296d31541c3c9938d205d3
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-19 10:27:52 -0500

    Switch Statements

---
+++ cmake-build-debug/CMakeFiles/switchStatements.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/switchStatements.dir/switchStatements.cpp.o"
+  "switchStatements"
+  "switchStatements.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/switchStatements.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
